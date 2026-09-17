fda8cf69840edec93ca83ffeff8af023220bba4d
akshay07-max <akshaysurase992@gmail.com>
2026-09-17 16:13:25 +0530

    data types

--- 01_data_types.py
+++ 01_data_types.py
@@ -5,5 +5,17 @@ a = 10
 print(a)
 print(type(a))
 
+b = 2.32
+print(b)
+print(type(b))
+
+
+c = 33+33j
+print(c)
+print(type(c))
+
 # 2] str = immutable. eg. "Hello"
+d = "Good Boy"
+print(d)
+print(type(d))
 
